178435985cdbe7aa979af3ac1ac4fb5b50b478ed
nikobatzi1990 <nicole.boci@gmail.com>
2023-04-13 13:03:57 +0900

    wip: configure and initialize firebase

---
+++ server/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW5IgBueoYDBcipAsGfJ9rbhOI9otJkN4",
+  authDomain: "meltdown-tracker-d113a.firebaseapp.com",
+  projectId: "meltdown-tracker-d113a",
+  storageBucket: "meltdown-tracker-d113a.appspot.com",
+  messagingSenderId: "798924452527",
+  appId: "1:798924452527:web:0a90e842a2e94f68035a5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
