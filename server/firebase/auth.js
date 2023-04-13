ae855ad49fcee23d5b003838e4729edc5d5dd627
nikobatzi1990 <nicole.boci@gmail.com>
2023-04-13 16:24:38 +0900

    feat: create handler functions for signup, login, logout

---
+++ server/firebase/auth.js
@@ -0,0 +1,36 @@
+const { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } = require("firebase/auth");
+
+module.exports = {
+  // signup
+  handleSignUp: async function (email, password) {
+    try {
+      let newUser = await createUserWithEmailAndPassword(auth, email, password);
+      return newUser.user;
+    } catch (error) {
+      console.log("SIGNUP ERROR: ", err);
+    }
+  },
+
+  // login
+  handleLogin: async function (email, password) {
+    try {
+      let user = await signInWithEmailAndPassword(auth, email, password);
+      return user;
+    } catch (error) {
+      console.log("LOGIN ERROR: ", err);
+      return undefined;
+    }
+  },
+
+  // signout
+  handleLogout: async function () {
+    try {
+      await signOut(auth).then(() => {
+        console.log("User is signed out");
+      }) 
+    } catch (error) {
+        console.log("LOGOUT:🤒 ", error);
+    }
+  }
+  
+}
--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,14 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Path to process.env file
+require("dotenv").config({
+  path: "./.env",
+});
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAW5IgBueoYDBcipAsGfJ9rbhOI9otJkN4",
-  authDomain: "meltdown-tracker-d113a.firebaseapp.com",
-  projectId: "meltdown-tracker-d113a",
-  storageBucket: "meltdown-tracker-d113a.appspot.com",
-  messagingSenderId: "798924452527",
-  appId: "1:798924452527:web:0a90e842a2e94f68035a5a"
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
