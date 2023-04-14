980996acde175e5687de1d70edf9abc2d8c3697d
Nicole <103890618+nikobatzi1990@users.noreply.github.com>
2023-04-14 18:25:11 +0900

    15 write endpoint for signup (#19)
    * wip: connect express to database
    
    * fix: name of Signup.jsx
    
    * feat: complete endpoint for signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+// import { useNavigate } from "react-router-dom";
 import "./Signup.css";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,24 +8,37 @@ import Input from '../components/Input';
 import Button from "../components/Button"
 
 const Signup = () => {
-  const [userName, setUsername] = useState('');
+
+  // const navigate = useNavigate();
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+    const newUserInfo = {
+      username: username,
+      email: email,
+      password: password,
+      timestamp: new Date()
+    }
+    const newUser = await axios.post('/api/signup', newUserInfo);
+    // console.log("🤌", newUser);
+  }
 
   return (
     <>
       <Header className = "header" text = "Signup"/>
 
       <div className="signup">
-      <Input 
+        <Input 
           htmlFor = "Username"
           className = "input"
           placeholder = "your nickname"
-          value = { userName }
+          value = { username }
           onChange = {(e) => {
             setUsername(e.target.value);
           }} />
-
         <Input 
           htmlFor = "Email"
           className = "input"
@@ -47,11 +61,10 @@ const Signup = () => {
           className = "submit"
           type = "submit"
           text="Submit"
+          onClick= { handleSignup }
           />
       </div>
-      <p>
-        Already have an account? <Link to = "/"> Log In! </Link>
-      </p>
+
       <Footer />
     </>
   );
