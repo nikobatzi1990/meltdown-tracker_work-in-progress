ef11a0344427b3dc0c58885c03adb39183276bc0
Nicole <103890618+nikobatzi1990@users.noreply.github.com>
2023-04-18 11:01:22 +0900

    22 deploy app on render (#37)
    * feat: add configuration for deployment
    
    * feat: add deployment scripts
    
    * fix: firebase variables
    
    * fix: remove line from index.js
    
    * feat: refactor environment variables

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -8,18 +8,12 @@ require("dotenv").config({
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FB_API_KEY,
-  // authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FB_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FB_APP_ID
-  apiKey: "AIzaSyAW5IgBueoYDBcipAsGfJ9rbhOI9otJkN4",
-  authDomain: "meltdown-tracker-d113a.firebaseapp.com",
-  projectId: "meltdown-tracker-d113a",
-  storageBucket: "meltdown-tracker-d113a.appspot.com",
-  messagingSenderId: "798924452527",
-  appId: "1:798924452527:web:0a90e842a2e94f68035a5a"
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID
 };
 
 // Initialize Firebase
