1af1cbd5060f7843796780e2d74fbe8c9531f62d
nikobatzi1990 <nicole.boci@gmail.com>
2023-04-18 11:20:02 +0900

    fix: import knex into index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const setUpServer = require('./server');
 const PORT = process.env.PORT || 8000;
 const server = setUpServer();
+const knex = require('../database/knex')
 
 (async () => {
   try {
-    // await knex.migrate.latest();
+    await knex.migrate.latest();
     server.listen(PORT, () => {
       console.log(`app is listening @ http://localhost:${PORT}`);
     })
