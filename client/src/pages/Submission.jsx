4707717e56a86f2864a7e15f251b896b8dd1cb5a
nikobatzi1990 <nicole.boci@gmail.com>
2023-04-19 15:58:56 +0900

    feat: add main components to Submission page

--- client/src/pages/Submission.jsx
+++ client/src/pages/Submission.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./Submission.css";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Submission = () => {
 
   return (
     <>
-      <Header className = "header" text = "Submission"/>
-      <Footer className = "footer" text = "© 2023 Meltown Tracker"/>
+      <Header className="header" text="Submission"/>
+
+      <div className="top">
+        <Input placeholder="Title"/>
+        <Input placeholder="Tag Search" />
+      </div>
+
+      <div className="center">
+        <div className="time-of-day">Time of Day</div>
+        <textarea className="entry-body"></textarea>
+
+      </div>
+
+      <Button text="Submit" />
+      <Footer className="footer" text="© 2023 Meltown Tracker"/>
     </>
   );
 };
