b428910a0826d0ecefbd93a7e4c5616f7d1cbe09
nikobatzi1990 <nicole.boci@gmail.com>
2023-04-19 18:05:42 +0900

    feat: add images for choosing time of day

--- client/src/pages/Submission.jsx
+++ client/src/pages/Submission.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 import "./Submission.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -6,6 +9,8 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 
 const Submission = () => {
+  const { user } = UserAuth();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -17,9 +22,22 @@ const Submission = () => {
       </div>
 
       <div className="center">
-        <div className="time-of-day">Time of Day</div>
-        <textarea className="entry-body"></textarea>
+        <div className="time-of-day">Time of Day
+          <figure>
+            <img src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1681888678/morning_rfereh.png" />
+            <figcaption>Morning</figcaption>
+          </figure>
+          <figure>
+            <img src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1681888682/afternoon_rdniws.png" />
+            <figcaption>Afternoon</figcaption>
+          </figure>
+          <figure>
+            <img src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1681888684/night_nubelk.png" />
+            <figcaption>Night</figcaption>
+          </figure>
+        </div>
 
+        <textarea className="entry-body"></textarea>
       </div>
 
       <Button text="Submit" />
