cf985b9bb7f1fec468a2e9d33868e7756d6e097a
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-05 19:47:59 +0900

    feat: add handlers and components

--- client/src/pages/EditSubmission.jsx
+++ client/src/pages/EditSubmission.jsx
@@ -13,7 +13,15 @@ import LightBulb from "../components/LightBulb";
 const EditSubmission = () => {
   const { user } = UserAuth();
   const navigate = useNavigate();
+
   const entryId = useParams();
+  const [entry, setEntry] = useState({});
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [tag, setTag] = useState("");
+  const [time, setTime] = useState("");
+  const [isFlagged, setIsFlagged] = useState(false);
+  const [classname, setClassname] = useState("");
 
   const getEntry = async () => {
   const fetchedEntry = await axios.get(`/api/entries/entry/${entryId.entryId}`);
@@ -29,12 +37,6 @@ const EditSubmission = () => {
     setIsFlagged(`${entry.flagged}`);
   }, []);
 
-  const [entry, setEntry] = useState({});
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [tag, setTag] = useState("");
-  const [time, setTime] = useState("");
-  const [isFlagged, setIsFlagged] = useState(false);
 
   const editedData = {
     uid: user.uid, 
@@ -45,13 +47,112 @@ const EditSubmission = () => {
     flagged: isFlagged,
   };
 
+  const handleTitleInput = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    setTitle(value);
+  }
+
+  const handleTextBody = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    setBody(value);
+  }
+
+  const handleTagInput = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    setTag(value);
+  }
+
+  const handleTimeOfDay = (event) => {
+    event.preventDefault();
+    const value = event.target.id;
+    setTime(value);
+  }
+
+  const handleInitialFlag = () => {
+    if (isFlagged === true) {
+      setClassname("filled material-symbols-outlined");
+    } else {
+      setClassname("material-symbols-outlined");
+    }
+  }
+
+  const handleFlag = (event) => {
+    event.preventDefault();
+    if (isFlagged === false) {
+      event.target.className = "filled material-symbols-outlined"
+      setIsFlagged(true);
+    } else {
+      event.target.className = "material-symbols-outlined"
+      setIsFlagged(false);
+    }
+  }
+
+  useEffect(() => {
+    handleInitialFlag();
+    console.log(classname);
+  }, []);
 
   return (
     <>
-      <Header className="header" text = "Meltdown Tracker"/>
+      <Header 
+        className="header entries-header" 
+        text="Meltdown Tracker"/>
 
-  
-      <Footer className="footer" text = "© 2023 Meltown Tracker"/>
+      <div className="main-body">
+
+        <TimeOfDay onClick={ handleTimeOfDay }/>
+        
+        <div className="submission">
+          <div className="top">
+            
+            <LightBulb 
+              className={ classname }
+              onClick={ handleFlag }
+              title="Was this a significant event?"
+               />
+
+            <Input 
+              className="input title-input"
+              placeholder="Title"
+              value={ title }
+              onChange={ handleTitleInput } />
+
+            <Input 
+              className="input"
+              placeholder="Tag"
+              value={ tag }
+              onChange={ handleTagInput } />
+          </div>
+          
+          <textarea 
+            className="entry-body"
+            placeholder="Type your entry here!"
+            value={ body }
+            cols="60" 
+            rows="30" 
+            onChange={ handleTextBody }>
+          </textarea>
+
+          <div className="submission__buttons">
+            <Button 
+              className="button"
+              text="Submit" 
+              />
+
+            <Button 
+              className="button"
+              text="Back to Entries" 
+              onClick={ () => {navigate('/entries')} } />
+          </div>
+        </div>
+      </div>
+
+      <Footer 
+      className="footer" 
+      text="© 2023 Meltown Tracker"/>
     </>
   );
 }
