404a40855327327bee879556cf44e632753cdd5c
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-12 10:11:59 +0900

    feat: add images to component

--- client/src/components/IntensityLevel.jsx
+++ client/src/components/IntensityLevel.jsx
@@ -11,6 +11,7 @@ const IntensityLevel = (props) => {
         type="image"
         id="1"
         className="intensity__input"
+        src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1694480953/number_1_glcplx.png"
         onClick={ onClick }
         alt="Level 1"
         title="Level 1" 
@@ -20,6 +21,7 @@ const IntensityLevel = (props) => {
         type="image"
         id="2"
         className="intensity__input"
+        src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1694481004/number_2_is4p8l.png"
         onClick={ onClick }
         alt="Level 2"
         title="Level 2"
@@ -29,6 +31,7 @@ const IntensityLevel = (props) => {
         type="image"
         id="3"
         className="intensity__input"
+        src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1694481007/number_3_zirrfq.png"
         onClick={ onClick }
         alt="Level 3"
         title="Level 3"
@@ -38,6 +41,7 @@ const IntensityLevel = (props) => {
         type="image"
         id="4"
         className="intensity__input"
+        src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1694481011/number_4_hrhmmx.png"
         onClick={ onClick }
         alt="Level 4"
         title="Level 4"
@@ -47,6 +51,7 @@ const IntensityLevel = (props) => {
         type="image"
         id="5"
         className="intensity__input"
+        src="https://res.cloudinary.com/dp2pjsbnz/image/upload/v1694481012/number_5_ra5e2q.png"
         onClick={ onClick }
         alt="Level 5"
         title="Level 5"
