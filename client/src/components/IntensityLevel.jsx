998a656dba81b2993f24427452a58dff3d6b3d10
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-12 09:18:08 +0900

    feat: create IntensityLevel component

---
+++ client/src/components/IntensityLevel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+const IntensityLevel = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="intensity-level" alt="Meltdown Intensity">
+      <p>Meltdown Intensity</p>
+      
+    </div>
+  )
+};
+
+export default IntensityLevel;
