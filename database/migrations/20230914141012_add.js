d6ab2b3aab4abc1d20999fd410b95f7df7a563b3
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-14 23:31:18 +0900

    feat: add migration for meltdown intensity

---
+++ database/migrations/20230914141012_add.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  knex.schema.alterTable('posts', function (table) {
+    table.string('intensity');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  knex.schema.table('posts', function (table) {
+    table.dropColumn('intensity');
+  });
+  // table.dropColumn(name)
+  
+};
