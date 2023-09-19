2a79bff7446a34f20acf25c913e9565d0f66569c
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-19 22:44:40 +0900

    feat: add function to edit intensity level

--- client/src/pages/EditSubmission.jsx
+++ client/src/pages/EditSubmission.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
-import { UserAuth } from "../context/AuthContext";
 import axios from 'axios';
 import './styles/EditSubmission.css';
 import Header from "../components/Header";
@@ -9,9 +8,9 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import TimeOfDay from "../components/TimeOfDay";
 import LightBulb from "../components/LightBulb";
+import IntensityLevel from "../components/IntensityLevel";
 
 const EditSubmission = () => {
-  const { user } = UserAuth();
   const navigate = useNavigate();
 
   const entryId = useParams();
@@ -20,6 +19,7 @@ const EditSubmission = () => {
   const [body, setBody] = useState("");
   const [tag, setTag] = useState("");
   const [time, setTime] = useState("");
+  const [intensity, setIntensity] = useState("not specified");
   const [isFlagged, setIsFlagged] = useState(false);
   const [classname, setClassname] = useState("");
 
@@ -27,7 +27,8 @@ const EditSubmission = () => {
     title: title, 
     body: body, 
     timeOfDay: time, 
-    flagged: isFlagged
+    flagged: isFlagged,
+    intensity: intensity
   };
 
   useEffect(() => {
@@ -44,6 +45,7 @@ const EditSubmission = () => {
     setTag(`${entry.tag_name}`);
     setTime(`${entry.time_of_day}`);
     setIsFlagged(`${entry.flagged}`);
+    setIntensity(`${entry.intensity}`);
   }, [entry]);
 
   const handleTitleInput = (event) => {
@@ -68,6 +70,12 @@ const EditSubmission = () => {
     event.preventDefault();
     const value = event.target.id;
     setTime(value);
+  }
+
+  const handleIntensity = (event) => {
+    event.preventDefault();
+    const value = event.target.id;
+    setIntensity(value);
   }
 
   const handleInitialFlag = () => {
@@ -96,7 +104,6 @@ const EditSubmission = () => {
 
   useEffect(() => {
     handleInitialFlag();
-    console.log(classname);
   }, []);
 
   return (
@@ -108,7 +115,8 @@ const EditSubmission = () => {
       <div className="main-body">
 
         <TimeOfDay onClick={ handleTimeOfDay }/>
-        
+        <IntensityLevel onClick={ handleIntensity }/>
+
         <div className="submission">
           <div className="top">
             
@@ -132,7 +140,7 @@ const EditSubmission = () => {
           </div>
           
           <textarea 
-            className="entry-body"
+            className="submission__body"
             placeholder="Type your entry here!"
             value={ body }
             cols="60" 
